b3c1d78565badeae8084c37c145aeaaa44b638c2
yangjc <bupt_yjc@163.com>
2018-08-14 16:13:06 +0800

    add server interface

--- web_service.py
+++ web_service.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser as parser
+from argparse import ArgumentParser
 
 import json
 
@@ -7,8 +7,20 @@ from flask import Flask, request
 from recognize import predict
 
 
+parser = ArgumentParser()
+parser.add_argument('-host', type=str, default='127.0.0.1')
+parser.add_argument('-port', type=str, default=2020)
+args = parser.parse_args()
+
 app = Flask(__name__)
 
 
+@app.route('/extract', methods=['POST'])
+def response():
+    data = request.get_json()
+    data['entity'] = predict(data['content'])
+    return json.dumps(data, ensure_ascii=False)
+
+
 if __name__ == '__main__':
-    ip = args.ip
+    app.run(args.host, args.port)
