e06e0370e1dfb12602b8660993c6c6619f5a6dd8
yangjc <bupt_yjc@163.com>
2018-09-04 19:28:13 +0800

    add shuffle

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import json
 import pickle as pk
 
+from random import shuffle
+
 from sklearn_crfsuite import CRF
 
 
@@ -12,6 +14,9 @@ def fit(path_sent, path_label, path_crf):
         sent_feats = json.load(f)
     with open(path_label, 'r') as f:
         labels = json.load(f)
+    sents_labels = list(zip(sent_feats, labels))
+    shuffle(sents_labels)
+    sent_feats, labels = zip(*sents_labels)
     crf = CRF(algorithm='lbfgs', min_freq=min_freq, c1=0.1, c2=0.1,
               max_iterations=100, all_possible_transitions=True)
     crf.fit(sent_feats, labels)
