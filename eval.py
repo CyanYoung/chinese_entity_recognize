a73c4a93908c9fe6226acd4413e3977f3e047918
yangjc <bupt_yjc@163.com>
2018-09-18 09:59:32 +0800

    change format

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@ import json
 
 from sklearn_crfsuite.metrics import flat_accuracy_score
 
-from recognize import restore_word, predict
+from recognize import predict
 
 
 def test(path):
@@ -11,29 +11,26 @@ def test(path):
     label_mat = list()
     pred_mat = list()
     error_mat = dict()
-    for triples in sents.values():
+    for text, triples in sents.items():
         labels = list()
-        preds = list()
         errors = list()
         for triple in triples:
             labels.append(triple['label'])
         label_mat.append(labels)
-        words = restore_word(triples)
-        text = ''.join(words)
         pairs = predict(text)
-        for word, pred in pairs:
-            preds.append(pred)
+        words = [word for word, pred in pairs]
+        preds = [pred for word, pred in pairs]
         pred_mat.append(preds)
         for word, pred, label in zip(words, preds, labels):
             if pred != label:
                 errors.append((word, label, pred))
         if errors:
             error_mat[text] = errors
-    print('%s %.2f\n' % ('acc:', flat_accuracy_score(label_mat, pred_mat)))
+    print('\n%s %.2f\n' % ('acc:', flat_accuracy_score(label_mat, pred_mat)))
     for text, errors in error_mat.items():
         error_str = text
         for word, label, pred in errors:
-            error_str = error_str + ', {}: {} -> {}'.format(word, label, pred)
+            error_str = error_str + ' | {}: {} -> {}'.format(word, label, pred)
         print(error_str)
 
 
