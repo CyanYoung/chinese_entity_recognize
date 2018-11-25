ced561c2d7d80cf189d2361615400440f24ca2f2
yangjc <bupt_yjc@163.com>
2018-11-25 09:57:32 +0800

    add bracket

--- start_service.sh
+++ start_service.sh
@@ -1,16 +1,16 @@
 usr=`whoami`
 
-if [ "$usr" == "cyan" ]
+if [[ "$usr" == "cyan" ]]
 then
     pid=`ps -ef | grep Python | grep web_interface | awk '{print $2}'`
-    if [ -n "$pid" ]
+    if [[ -n "$pid" ]]
     then
         kill ${pid}
     fi
     nohup python3 web_interface.py >/dev/null 2>&1 &
 else
     pid=`ps -ef | grep python | grep web_interface | awk '{print $2}'`
-    if [ -n "$pid" ]
+    if [[ -n "$pid" ]]
     then
         kill ${pid}
     fi
