a01e482b14a694daa46338e0be80526132f63371
yangjc <bupt_yjc@163.com>
2019-01-18 19:18:22 +0800

    change struct

--- preprocess.py
+++ preprocess.py
@@ -8,14 +8,16 @@ import re
 import jieba
 from jieba.posseg import cut as pos_cut
 
-from util import load_word, load_word_re, load_pair
+from random import shuffle, choice
+
+from util import load_word, load_pair, load_poly, flat_read
 
 
 def save_entity(path_train_dir, path_entity):
     entity_set = set()
     files = os.listdir(path_train_dir)
     for file in files:
-        entity_strs = pd.read_csv(os.path.join(path_train_dir, file), usecols=['entity']).values
+        entity_strs = flat_read(os.path.join(path_train_dir, file), 'entity')
         for entity_str in entity_strs:
             entitys = entity_str[0].strip().split()
             for entity in entitys:
@@ -32,61 +34,147 @@ def add_entity(path_word, path_entity):
             f.write(word + '\n')
 
 
-path_train_dir = 'data/train'
-path_entity = 'dict/entity.txt'
-path_name = 'dict/name.txt'
 path_zh_en = 'dict/zh_en.csv'
-path_stop_word = 'dict/stop_word.txt'
-save_entity(path_train_dir, path_entity)
-add_entity(path_name, path_entity)
-jieba.load_userdict(path_entity)
+path_homo = 'dict/homo.csv'
+path_syno = 'dict/syno.csv'
 zh_en = load_pair(path_zh_en)
-stop_word_re = load_word_re(path_stop_word)
+homo_dict = load_poly(path_homo)
+syno_dict = load_poly(path_syno)
 
 
-def label_word(text, entitys, label):
-    triples = list()
-    pairs = list(pos_cut(text))
-    for word, pos in pairs:
-        triple = dict()
-        triple['word'] = word
-        triple['pos'] = pos
-        if triple['word'] in entitys:
-            triple['label'] = label
-        else:
-            triple['label'] = 'O'
-        triples.append(triple)
-    return triples
-
-
-def merge(sents, text, triples):
-    pre_triples = sents[text]
-    for triple, pre_triple in zip(triples, pre_triples):
-        if triple['label'] != 'O' and pre_triple['label'] == 'O':
-            pre_triple['label'] = triple['label']
-
-
-def prepare(path, path_dir):
-    sents = dict()
-    files = os.listdir(path_dir)
-    for file in files:
-        label = zh_en[os.path.splitext(file)[0]]
-        for text, entity_str in pd.read_csv(os.path.join(path_dir, file)).values:
-            text = re.sub(stop_word_re, '', text.strip())
-            entitys = entity_str.strip().split()
-            triples = label_word(text, entitys, label)
-            if text in sents:
-                merge(sents, text, triples)
-            else:
-                sents[text] = triples
+def save(path, sents):
     with open(path, 'w') as f:
         json.dump(sents, f, ensure_ascii=False, indent=4)
 
 
+def dict2list(sents):
+    word_mat, label_mat = list(), list()
+    for pairs in sents.values():
+        words, labels = list(), list()
+        for pair in pairs:
+            words.append(pair['word'])
+            labels.append(pair['label'])
+        word_mat.append(words)
+        label_mat.append(labels)
+    return word_mat, label_mat
+
+
+def list2dict(word_mat, label_mat):
+    sents = dict()
+    for words, labels in zip(word_mat, label_mat):
+        text = ''.join(words)
+        pairs = list()
+        for word, label in zip(words, labels):
+            pair = dict()
+            pair['word'] = word
+            pair['label'] = label
+            pairs.append(pair)
+        sents[text] = pairs
+    return sents
+
+
+def select(part):
+    if part[0] == '[' and part[-1] == ']':
+        word = part[1:-1]
+        cands = set()
+        cands.add(word)
+        if word in syno_dict:
+            cands.update(syno_dict[word])
+        if word in homo_dict:
+            cands.update(homo_dict[word])
+        return choice(list(cands))
+    elif part[0] == '(' and part[-1] == ')':
+        word = part[1:-1]
+        return choice([word, ''])
+    else:
+        return part
+
+
+def generate(temps, slots, num):
+    word_mat, label_mat = list(), list()
+    for i in range(num):
+        parts = choice(temps)
+        words, labels = list(), list()
+        for part in parts:
+            if part in slots:
+                entity = choice(slots[part])
+                words.append(entity)
+                labels.append(part)
+            else:
+                word = select(part)
+                words.append(word)
+                labels.append('O')
+        word_mat.append(words)
+        label_mat.append(labels)
+    return word_mat, label_mat
+
+
+def sync_shuffle(list1, list2):
+    pairs = list(zip(list1, list2))
+    shuffle(pairs)
+    return zip(*pairs)
+
+
+def label_sent(path):
+    sents = dict()
+    for text, entity_str, label_str in pd.read_csv(path).values:
+        pairs = list(pos_cut(text))
+        entitys, labels = entity_str.split(), label_str.split()
+        if len(entitys) != len(labels):
+            print('skip: %s', text)
+            continue
+        triples = list()
+        for word, pos in pairs:
+            triple = dict()
+            triple['word'] = word
+            triple['pos'] = pos
+            if word in entitys:
+                ind = entitys.index(word)
+                triple['label'] = labels[ind]
+            else:
+                triple['label'] = 'O'
+            triples.append(triple)
+        sents[text] = triples
+    return sents
+
+
+def expand(sents, gen_word_mat, gen_label_mat):
+    word_mat, label_mat = dict2list(sents)
+    word_mat.extend(gen_word_mat)
+    label_mat.extend(gen_label_mat)
+    word_mat, label_mat = sync_shuffle(word_mat, label_mat)
+    bound = int(len(word_mat) * 0.9)
+    train_sents = list2dict(word_mat[:bound], label_mat[:bound])
+    test_sents = list2dict(word_mat[bound:], label_mat[bound:])
+    return train_sents, test_sents
+
+
+def prepare(paths):
+    temps = list()
+    with open(paths['temp'], 'r') as f:
+        for line in f:
+            parts = line.strip().split()
+            temps.append(parts)
+    slots = dict()
+    files = os.listdir(paths['slot_dir'])
+    for file in files:
+        label = zh_en[os.path.splitext(file)[0]]
+        slots[label] = list()
+        with open(os.path.join(paths['slot_dir'], file), 'r') as f:
+            for line in f:
+                slots[label].append(line.strip())
+    gen_word_mat, gen_label_mat = generate(temps, slots, num=5000)
+    sents = label_sent(paths['extra'])
+    train_sents, test_sents = expand(sents, gen_word_mat, gen_label_mat)
+    save(paths['train'], train_sents)
+    save(paths['test'], test_sents)
+
+
 if __name__ == '__main__':
-    path = 'data/train.json'
-    path_dir = 'data/train'
-    prepare(path, path_dir)
-    path = 'data/test.json'
-    path_dir = 'data/test'
-    prepare(path, path_dir)
+    paths = dict()
+    paths['train'] = 'data/train.json'
+    paths['test'] = 'data/test.json'
+    paths['temp'] = 'data/template.txt'
+    paths['slot_dir'] = 'data/slot'
+    paths['extra'] = 'data/extra.csv'
+    prepare(paths)
