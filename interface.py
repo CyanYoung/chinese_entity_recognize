33e7642994afed60cc1478e707d1120ff1936593
yangjc <bupt_yjc@163.com>
2019-01-18 14:56:59 +0800

    simplify interface

--- interface.py
+++ interface.py
@@ -6,13 +6,11 @@ from argparse import ArgumentParser
 
 from recognize import predict
 
-from util import load_pair, load_triple, get_logger
+from util import load_pair, get_logger
 
 
 path_zh_en = 'dict/zh_en.csv'
-path_label_key_slot = 'dict/label_key_slot.csv'
 zh_en = load_pair(path_zh_en)
-label_key_slot = load_triple(path_label_key_slot)
 
 app = Flask(__name__)
 
@@ -25,20 +23,12 @@ path_log_dir = 'log'
 logger = get_logger('recognize', path_log_dir)
 
 
-def map_slot(word, pred):
-    for label, key, slot in label_key_slot:
-        if pred == label:
-            if key in word:
-                return slot
-    return pred
-
-
-def make_dict(entitys, slots):
+def make_dict(entitys, labels):
     slot_dict = dict()
-    for slot, entity in zip(slots, entitys):
-        if slot not in slot_dict:
-            slot_dict[slot] = list()
-        slot_dict[slot].append(entity)
+    for label, entity in zip(labels, entitys):
+        if label not in slot_dict:
+            slot_dict[label] = list()
+        slot_dict[label].append(entity)
     return slot_dict
 
 
@@ -46,13 +36,12 @@ def make_dict(entitys, slots):
 def response():
     data = request.get_json()
     pairs = predict(data['content'])
-    entitys, slots = list(), list()
+    entitys, labels = list(), list()
     for word, pred in pairs:
         if pred != 'O':
             entitys.append(word)
-            slot = map_slot(word, zh_en[pred])
-            slots.append(slot)
-    slot_dict = make_dict(entitys, slots)
+            labels.append(zh_en[pred])
+    slot_dict = make_dict(entitys, labels)
     data['slot'] = slot_dict
     data_str = json.dumps(data, ensure_ascii=False)
     logger.info(data_str)
