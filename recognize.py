79ee9225427b7cccefd80ebe956316cead48601e
yangjc <bupt_yjc@163.com>
2019-01-19 11:01:15 +0800

    fix predict

--- recognize.py
+++ recognize.py
@@ -1,11 +1,13 @@
 import pickle as pk
 
-from preprocess import label_word
+import jieba
+from jieba.posseg import cut as pos_cut
 
 from represent import sent2feat
 
-
+path_cut_word = 'dict/cut_word.txt'
 path_crf = 'model/crf.pkl'
+jieba.load_userdict(path_cut_word)
 with open(path_crf, 'rb') as f:
     crf = pk.load(f)
 
@@ -19,8 +21,15 @@ def restore(triples):
 
 def predict(text):
     text = text.strip()
-    triples = label_word(text, [], '')
-    words = restore(triples)
+    pairs = list(pos_cut(text))
+    words = [word for word, tag in pairs]
+    tags = [tag for word, tag in pairs]
+    triples = list()
+    for word, tag in zip(words, tags):
+        triple = dict()
+        triple['word'] = word
+        triple['pos'] = tag
+        triples.append(triple)
     sent_feat = sent2feat(triples)
     preds = crf.predict([sent_feat])[0]
     pairs = list()
