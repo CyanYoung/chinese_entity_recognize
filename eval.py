805383856d2ddfbcdfbb0f8d210797c3d8937ffc
yangjc <bupt_yjc@163.com>
2019-05-05 12:29:14 +0800

    fix ind

--- eval.py
+++ eval.py
@@ -23,10 +23,10 @@ with open(path_label_ind, 'rb') as f:
 
 class_num = len(label_inds)
 
-slots = list(label_inds.keys())
-slots.remove('O')
-
 ind_labels = ind2label(label_inds)
+
+slots = list(ind_labels.keys())
+slots.remove(label_inds['O'])
 
 path_crf = 'metric/crf.csv'
 
@@ -41,13 +41,13 @@ def test(sents):
         word2s, preds = predict(text)
         for i in range(len(word2s)):
             if word2s[i] == word2s[i]:
-                flat_labels.append(labels[i])
-                flat_preds.append(preds[i])
+                flat_labels.append(label_inds[labels[i]])
+                flat_preds.append(label_inds[preds[i]])
     precs = precision_score(flat_labels, flat_preds, average=None)
     recs = recall_score(flat_labels, flat_preds, average=None)
     with open(path_crf, 'w') as f:
         f.write('label,prec,rec' + '\n')
-        for i in range(1, class_num):
+        for i in range(class_num):
             f.write('%s,%.2f,%.2f\n' % (ind_labels[i], precs[i], recs[i]))
     f1 = f1_score(flat_labels, flat_preds, average='weighted', labels=slots)
     print('\n%s f1: %.2f - acc: %.2f' % ('crf', f1, accuracy_score(flat_labels, flat_preds)))
